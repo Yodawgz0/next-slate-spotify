29b0ca104c54bc4d27b75d1a1f1998056c043f14
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-09-16 10:26:08 +0530

    adding Icons

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,9 +1,36 @@
 import type { NextPage } from "next";
+import {
+  HomeIcon,
+  MagnifyingGlassIcon,
+  TableCellsIcon,
+} from "@heroicons/react/24/solid";
 
 const SidebBar: NextPage = () => {
   return (
     <>
-      <div className="text-2xl font-bold underline"> This is Sidevar</div>
+      <div className="p-5 space-y-6 mt-3">
+        <div className="flex ml-5 text-[#dfedd9] hover:text-white">
+          {" "}
+          <button>
+            <HomeIcon className="h-5 w-5 mr-2 " />
+          </button>
+          <p className="font-bold">Home</p>
+        </div>
+        <div className="flex ml-5 text-[#dfedd9] hover:text-white">
+          {" "}
+          <button>
+            <MagnifyingGlassIcon className="h-5 w-5 mr-2" />
+          </button>
+          <p className="font-bold">Search</p>
+        </div>
+        <div className="flex ml-5 text-[#dfedd9] hover:text-white">
+          {" "}
+          <button>
+            <TableCellsIcon className="h-5 w-5 mr-2" />
+          </button>
+          <p className="font-bold">Your Library</p>
+        </div>
+      </div>
     </>
   );
 };
